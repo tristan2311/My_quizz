9bbee106d76bbffb804869fe44913c887ec69ee5
tristan2311 <tristan.letoumelin2@edu.esiee.fr>
2022-11-15 17:21:24 +0100

    Initial commit

---
+++ app/src/main/java/com/example/my_quizz/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.my_quizz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mUpperTextView;
+    private EditText mNameEditText;
+    private Button mStartButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

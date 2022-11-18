1a65d0b0e8f5dd84c4d153921a768f401cd94e29
tristan2311 <tristan.letoumelin2@edu.esiee.fr>
2022-11-18 14:21:29 +0100

    2eme commit (add an activity)

--- app/src/main/java/com/example/my_quizz/MainActivity.java
+++ app/src/main/java/com/example/my_quizz/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.my_quizz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,5 +19,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNameEditText = findViewById(R.id.main_challenger_name);
+        mStartButton = findViewById(R.id.main_start_button);
+        mStartButton.setEnabled(false); //permet de désactiver le bouton
+
+        mNameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                mStartButton.setEnabled(!s.toString().isEmpty()); //détecte si il y a au moins une lettre de rentrée
+            }
+        });
     }
 }

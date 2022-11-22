04cfa0eb8892b6b2e8801c386ca7b97f11926934
Lars78720 <mathieu.lionnet@edu.esiee.fr>
2022-11-22 11:56:29 +0100

    Version Mathieu

---
+++ app/src/main/java/com/example/my_quizz/QuestionAnswer.java
@@ -0,0 +1,22 @@
+package com.example.my_quizz;
+
+public class QuestionAnswer {
+    public static String question[] ={
+            "Quelle est la capitale de la France?",
+            "Quelle est la capitale de l'Espagne?",
+            "Quelle est la capitale de la Belgique?"
+
+    };
+    public static String choix[][] ={
+            {"Berlin","Paris","Madrid","Mexico"},
+            {"Paris","Rome","Madrid","Paris"},
+            {"Bruxelles","Belgrade","Athènes","Amsterdam"}
+
+    };
+    public static String Bonnereponse[] = {
+            "Paris",
+            "Madrid",
+            "Bruxelles"
+
+    };
+}

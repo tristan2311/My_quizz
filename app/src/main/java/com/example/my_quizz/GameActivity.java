519465e70ed13d3e14c23522fadaa37010458b7b
Lars78720 <mathieu.lionnet@edu.esiee.fr>
2022-11-22 11:54:40 +0100

    Version Mathieu

--- app/src/main/java/com/example/my_quizz/GameActivity.java
+++ app/src/main/java/com/example/my_quizz/GameActivity.java
@@ -1,30 +1,108 @@
 package com.example.my_quizz;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
+
+import android.app.AlertDialog;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 
-public class GameActivity extends AppCompatActivity{
+public class GameActivity extends AppCompatActivity implements View.OnClickListener{
+    TextView totalQuestionsTextView;
+    TextView questionTextView;
+    Button repA,repB,repC,repD;
 
-    Button mAnswer1Button;
-    Button mAnswer2Button;
-    Button mAnswer3Button;
-    Button mAnswer4Button;
+    int score = 0;
+    int totalQuestion = QuestionAnswer.question.length;
+    int currentQuestionIndex = 0;
+    String selectedAnswer="";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_in_game);
+        totalQuestionsTextView = findViewById(R.id.total_question);
+        questionTextView = findViewById(R.id.question);
 
-        mAnswer1Button = findViewById((R.id.in_game_activity_button_1));
-        mAnswer2Button = findViewById((R.id.in_game_activity_button_2));
-        mAnswer3Button = findViewById((R.id.in_game_activity_button_3));
-        mAnswer4Button = findViewById((R.id.in_game_activity_button_4));
+        repA = findViewById((R.id.in_game_activity_button_1));
+        repB = findViewById((R.id.in_game_activity_button_2));
+        repC = findViewById((R.id.in_game_activity_button_3));
+        repD = findViewById((R.id.in_game_activity_button_4));
+
+        repA.setOnClickListener(this);
+        repB.setOnClickListener(this);
+        repC.setOnClickListener(this);
+        repD.setOnClickListener(this);
+
+        totalQuestionsTextView.setText("Total questions : "+totalQuestion);
+        loadNewQuestion();
+    }
+    @Override
+    public void onClick(View view) {
+        Button clickedButton = (Button) view;
+        if (clickedButton.getId()==R.id.in_game_activity_button_1) {
+            if(selectedAnswer.equals(QuestionAnswer.Bonnereponse[currentQuestionIndex])){
+                score ++;
+            }
+        if (clickedButton.getId()==R.id.in_game_activity_button_2)
+            if(selectedAnswer.equals(QuestionAnswer.Bonnereponse[currentQuestionIndex])) {
+                score++;
+            }
+        if(clickedButton.getId()==R.id.in_game_activity_button_3){
+            if (selectedAnswer.equals(QuestionAnswer.Bonnereponse[currentQuestionIndex])){
+                score++;
+            }
+        if(clickedButton.getId()==R.id.in_game_activity_button_4){
+            if(selectedAnswer.equals(QuestionAnswer.Bonnereponse[currentQuestionIndex])){
+                score++;
+            }
+        }
+            currentQuestionIndex++;
+            loadNewQuestion();
+
+
+        }else{
+            selectedAnswer = clickedButton.getText().toString();
+        }
+
+    }
+    }
+
+    private void loadNewQuestion() {
+        if(currentQuestionIndex == totalQuestion){
+            finishQuiz();
+            return;
+        }
+        questionTextView.setText(QuestionAnswer.question[currentQuestionIndex]);
+        repA.setText(QuestionAnswer.choix[currentQuestionIndex][0]);
+        repB.setText(QuestionAnswer.choix[currentQuestionIndex][1]);
+        repC.setText(QuestionAnswer.choix[currentQuestionIndex][2]);
+        repD.setText(QuestionAnswer.choix[currentQuestionIndex][3]);
+    }
+
+    private void finishQuiz() {
+        String passStatus = "";
+        if (score > totalQuestion*0.60) {
+            passStatus = "Passed";
+        }else{
+            passStatus = "Failed";
+        }
+
+        new AlertDialog.Builder(this)
+                .setTitle(passStatus)
+                .setMessage("Score"+score+"sur"+totalQuestion)
+                .setPositiveButton("Recommencer",((dialogInterface, i) -> restartQuiz()))
+                .setCancelable(false)
+                .show();
+
+    }
+
+    private void restartQuiz() {
+        score=0;
+        currentQuestionIndex=0;
+        loadNewQuestion();
 
     }
 }

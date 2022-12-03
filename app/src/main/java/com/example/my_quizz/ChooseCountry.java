3ccccebb17a73d0cdbab4c47220998f30eb942ba
captair <project.captair@gmail.com>
2022-12-03 16:58:27 +0100

    choix du continent avec bouton qui est une image (Fabien)

--- app/src/main/java/com/example/my_quizz/ChooseCountry.java
+++ app/src/main/java/com/example/my_quizz/ChooseCountry.java
@@ -1,9 +1,11 @@
 package com.example.my_quizz;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,9 +15,10 @@ import java.util.Map;
 public class ChooseCountry extends AppCompatActivity implements View.OnClickListener{
 
     public static Map<Integer, String[][]> CountryAnswer = new HashMap<Integer, String[][]>();
-    Button ButtonAmerique;
-    Button ButtonEurope;
+    ImageButton ButtonAmerique;
+    ImageButton ButtonEurope;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +32,7 @@ public class ChooseCountry extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View view) {        // The user just clicked
-        Button clickedButton = (Button) view;
+        ImageButton clickedButton = (ImageButton) view;
         switch(clickedButton.getId()) {
             case R.id.buttonEurope:
                 CountryAnswer = QuestionAnswer.EuropeAnswer;
